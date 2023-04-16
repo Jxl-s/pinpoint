0be30441a0074699e67a7acdc7f6112023f2b3b3
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-04-16 15:36:48 -0400

    added nearby page on blue

--- lib/blue/map_screen.dart
+++ lib/blue/map_screen.dart
@@ -55,7 +55,112 @@ class MapScreen extends PinPointScreen {
   }
 
   Widget NearbyPage() {
-    return Placeholder();
+    return ListView(
+      children: [
+        NearbyCard(
+          type: "College",
+          distance: "200 m",
+          name: "Vanier College",
+          address: "821 Sainte Croix Ave, Saint-Laurent, Quebec H4L 3X9",
+        ),
+        NearbyCard(
+          type: "Pizza Restaurant",
+          distance: "3.2 km",
+          name: "Papa John's Pizza",
+          address: "1320 De l'Église St, Saint-Laurent, Quebec H4L 2G7",
+        ),
+        NearbyCard(
+          type: "Subway Station",
+          distance: "33.3 km",
+          name: "Metro Cote Vertu",
+          address: "1010 Boulevard Cote Vertu Ouest, Saint-Laurent, Quebec H4L",
+        ),
+      ],
+    );
+  }
+
+  Widget NearbyCard({
+    required String type,
+    required String distance,
+    required String name,
+    required String address,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Container(
+        padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(color: Colors.black.withOpacity(0.1)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 8,
+                      height: 8,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      type,
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  distance,
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, bottom: 4),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Text(
+              address,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'VIEW ON MAP',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget MapPage() {
