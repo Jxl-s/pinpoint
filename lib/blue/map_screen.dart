c59b5aeaec4969c8ba821b9b4a7770dc9f332ce9
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-04-16 15:13:01 -0400

    map page bottom bar

--- lib/blue/map_screen.dart
+++ lib/blue/map_screen.dart
@@ -42,34 +42,11 @@ class MapScreen extends PinPointScreen {
           width: double.infinity,
           height: double.infinity,
           padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+          child: TabBarView(
             children: [
-              Text(
-                "Welcome to PinPoint",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
-              ),
-              Text(
-                "Never forget your favourite locations. "
-                "Pin them and come back at a later time.",
-                style: TextStyle(
-                  fontWeight: FontWeight.w100,
-                  fontSize: 20,
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    signinButton(provider: "Google", onPressed: () {}),
-                    signinButton(provider: "Facebook", onPressed: () {}),
-                    signinButton(provider: "Discord", onPressed: () {}),
-                  ],
-                ),
-              ),
+              NearbyPage(),
+              MapPage(),
+              PinsPage(),
             ],
           ),
         ),
@@ -77,20 +54,15 @@ class MapScreen extends PinPointScreen {
     );
   }
 
-  Widget signinButton(
-      {required String provider, required void Function() onPressed}) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        width: double.infinity,
-        child: TextButton(
-          onPressed: onPressed,
-          child: Text(
-            "SIGN IN WITH ${provider.toUpperCase()}",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-        ),
-      ),
-    );
+  Widget NearbyPage() {
+    return Placeholder();
+  }
+
+  Widget MapPage() {
+    return Placeholder();
+  }
+
+  Widget PinsPage() {
+    return Placeholder();
   }
 }
