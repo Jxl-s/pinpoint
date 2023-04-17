e9b36505c5ce8c4a547bad07ed60d96bf5c00b28
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-04-16 23:00:54 -0400

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:pinpoint/main_blue.dart';
-import './landing_screen.dart';
-import './nearby_screen.dart';
-import './map_screen.dart';
-import './search_screen.dart';
+import 'package:pinpoint/blue/map_screen.dart';
 
 void main() {
-  runApp(MainBlue());
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  Main({super.key});
+
+  final Widget screen = MapScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PinPoint',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Poppins",
+      ),
+      home: screen,
+    );
+  }
 }
