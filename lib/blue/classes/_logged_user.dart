cb34caecfa60735e5b6a7a0db5f9c8035c199ab9
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-04-17 23:10:59 -0400

    add auth things

--- lib/blue/classes/_logged_user.dart
+++ lib/blue/classes/_logged_user.dart
@@ -5,12 +5,12 @@ User? loggedUser = null;
 User? getLoggedUser() {
   if (loggedUser == null) {
     // try to fetch the user
-    loggedUser ??= User(
-      name: 'Jia',
-      email: 'jia@jia.com',
-      avatar:
-          'https://lh3.googleusercontent.com/ogw/AOLn63G9zxI6m2QCJv8VMG_nBCeMOUIFEgTZwI2dTKOGUA=s32-c-mo',
-    );
+    // loggedUser ??= User(
+    //   name: 'Jia',
+    //   email: 'jia@jia.com',
+    //   avatar:
+    //       'https://lh3.googleusercontent.com/ogw/AOLn63G9zxI6m2QCJv8VMG_nBCeMOUIFEgTZwI2dTKOGUA=s32-c-mo',
+    // );
   }
 
   return loggedUser;
