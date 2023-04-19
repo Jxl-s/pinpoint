749a162ba4b1259d6a7db57d84e9d9c064ad2b24
%GoMerKs% <skykings98657@gmail.com>
2023-04-19 06:26:29 -0400

    Added template for Messages Screen

---
+++ lib/blue/messages_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pinpoint/blue/classes/user.dart';
+import 'package:pinpoint/blue/components/confirm_dialog.dart';
+import 'package:pinpoint/blue/services/auth.dart';
+import './components/drawer.dart';
+
+class MessagesScreen extends StatefulWidget {
+  const MessagesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MessagesScreen> createState() => _MessagesScreenState();
+}
+
+class _MessagesScreenState extends State<MessagesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
