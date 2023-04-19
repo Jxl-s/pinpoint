05a9d7ee5d2c41a4e1e59ad2926f361799acce5c
%GoMerKs% <skykings98657@gmail.com>
2023-04-19 05:13:07 -0400

    added the body for settings

--- lib/blue/settings_screen.dart
+++ lib/blue/settings_screen.dart
@@ -24,8 +24,27 @@ class _SettingsScreenState extends State<SettingsScreen> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: settingsMenu(),
       ),
     );
+  }
+
+  Widget settingsMenu() {
+    return Container();
   }
 }
