5f091bf9f0826c252efc91e2bf0edb10bc1443df
%GoMerKs% <skykings98657@gmail.com>
2023-04-19 04:57:47 -0400

    added the drawer in settings

---
+++ lib/blue/settings_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:pinpoint/blue/classes/user.dart';
+import 'package:pinpoint/blue/components/confirm_dialog.dart';
+import 'package:pinpoint/blue/services/auth.dart';
+import './components/drawer.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: PinPointDrawer(
+        title: "Settings",
+      ),
+    );
+  }
+}
